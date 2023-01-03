0159ec50d823131ea30352d5c4ec5524e5d1cbe0
caramelribon <caramelribon137@gmail.com>
2023-01-03 22:13:58 +0900

    styled-componentsの設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    // styledComponentsの有効化
+    styledComponents,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
